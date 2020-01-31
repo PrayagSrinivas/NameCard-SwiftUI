a4942db816d872054ef808fed8c8eb8233bdf6fa
sidhartha das <jovialwithigit@gmail.com>
2020-01-31 13:00:15 +0530

    Initial Commit

---
+++ NameCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NameCard
+//
+//  Created by Jovial Software on 11/11/1941 Saka.
+//  Copyright © 1941 Jovial Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
